ea59e78b4345f895c4f8027c4e415f4cdcf312fb
Sahar Sheikholeslami <84471457+SaharaSheik@users.noreply.github.com>
2022-08-19 00:18:53 -0400

    Add files via upload

---
+++ lex-1.c
@@ -0,0 +1,174 @@
+/*
+	This is the skeleton lex.c for the UCF Fall 2021 Systems Software 
+	Project, HW4. Add whatever you want, but make sure to leave the
+	print functions as they are to make sure you get your grade as
+	quickly as possible. There is a line commented out just before
+	the return statement in lexanalyzer(). Make sure to uncomment it
+	after you implement the function, your parser.c may malfunction
+	without it.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "compiler.h"
+#define MAX_NUMBER_TOKENS 1000
+#define MAX_IDENT_LEN 11
+#define MAX_NUMBER_LEN 5
+
+
+lexeme *list;
+int lex_index;
+
+void printlexerror(int type);
+void printtokens();
+
+
+lexeme *lexanalyzer(char *input, int printFlag)
+{
+	list = NULL;
+	
+	// put your lexical analysis code here
+	
+	if (printFlag)
+		printtokens();
+	// list[lex_index++].type = -1;
+	return list;
+}
+
+void printtokens()
+{
+	int i;
+	printf("Lexeme Table:\n");
+	printf("lexeme\t\ttoken type\n");
+	for (i = 0; i < lex_index; i++)
+	{
+		switch (list[i].type)
+		{
+			case oddsym:
+				printf("%11s\t%d", "odd", oddsym);
+				break;
+			case eqlsym:
+				printf("%11s\t%d", "=", eqlsym);
+				break;
+			case neqsym:
+				printf("%11s\t%d", "!=", neqsym);
+				break;
+			case lsssym:
+				printf("%11s\t%d", "<", lsssym);
+				break;
+			case leqsym:
+				printf("%11s\t%d", "<=", leqsym);
+				break;
+			case gtrsym:
+				printf("%11s\t%d", ">", gtrsym);
+				break;
+			case geqsym:
+				printf("%11s\t%d", ">=", geqsym);
+				break;
+			case modsym:
+				printf("%11s\t%d", "%", modsym);
+				break;
+			case multsym:
+				printf("%11s\t%d", "*", multsym);
+				break;
+			case divsym:
+				printf("%11s\t%d", "/", divsym);
+				break;
+			case addsym:
+				printf("%11s\t%d", "+", addsym);
+				break;
+			case subsym:
+				printf("%11s\t%d", "-", subsym);
+				break;
+			case lparensym:
+				printf("%11s\t%d", "(", lparensym);
+				break;
+			case rparensym:
+				printf("%11s\t%d", ")", rparensym);
+				break;
+			case commasym:
+				printf("%11s\t%d", ",", commasym);
+				break;
+			case periodsym:
+				printf("%11s\t%d", ".", periodsym);
+				break;
+			case semicolonsym:
+				printf("%11s\t%d", ";", semicolonsym);
+				break;
+			case assignsym:
+				printf("%11s\t%d", ":=", assignsym);
+				break;
+			case odsym:
+				printf("%11s\t%d", "od", odsym);
+				break;
+			case whensym:
+				printf("%11s\t%d", "when", whensym);
+				break;
+			case elsedosym:
+				printf("%11s\t%d", "elsedo", elsedosym);
+				break;
+			case whilesym:
+				printf("%11s\t%d", "while", whilesym);
+				break;
+			case dosym:
+				printf("%11s\t%d", "do", dosym);
+				break;
+			case callsym:
+				printf("%11s\t%d", "call", callsym);
+				break;
+			case writesym:
+				printf("%11s\t%d", "write", writesym);
+				break;
+			case readsym:
+				printf("%11s\t%d", "read", readsym);
+				break;
+			case constsym:
+				printf("%11s\t%d", "const", constsym);
+				break;
+			case varsym:
+				printf("%11s\t%d", "var", varsym);
+				break;
+			case procsym:
+				printf("%11s\t%d", "procedure", procsym);
+				break;
+			case identsym:
+				printf("%11s\t%d", list[i].name, identsym);
+				break;
+			case numbersym:
+				printf("%11d\t%d", list[i].value, numbersym);
+				break;
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("Token List:\n");
+	for (i = 0; i < lex_index; i++)
+	{
+		if (list[i].type == numbersym)
+			printf("%d %d ", numbersym, list[i].value);
+		else if (list[i].type == identsym)
+			printf("%d %s ", identsym, list[i].name);
+		else
+			printf("%d ", list[i].type);
+	}
+	printf("\n");
+}
+
+void printlexerror(int type)
+{
+	if (type == 1)
+		printf("Lexical Analyzer Error: Invalid Symbol\n");
+	else if (type == 2)
+		printf("Lexical Analyzer Error: Invalid Identifier\n");
+	else if (type == 3)
+		printf("Lexical Analyzer Error: Excessive Number Length\n");
+	else if (type == 4)
+		printf("Lexical Analyzer Error: Excessive Identifier Length\n");
+	else
+		printf("Implementation Error: Unrecognized Error Type\n");
+	
+	free(list);
+	return;
+}
---
+++ vm-1.c
@@ -0,0 +1,73 @@
+/*
+	This is the skeleton vm.c for the UCF Fall 2021 Systems Software 
+	Project, HW4. Add whatever you want, but make sure to leave the
+	print functions as they are to make sure you get your grade as
+	quickly as possible. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "compiler.h"
+#define MAX_PAS_LENGTH 3000
+
+void print_execution(int line, char *opname, int *IR, int PC, int BP, int SP, int DP, int *pas, int GP)
+{
+	int i;
+	// print out instruction and registers
+	printf("%2d\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t", line, opname, IR[1], IR[2], PC, BP, SP, DP);
+	
+	// print data section
+	for (i = GP; i <= DP; i++)
+		printf("%d ", pas[i]);
+	printf("\n");
+	
+	// print stack
+	printf("\tstack : ");
+	for (i = MAX_PAS_LENGTH - 1; i >= SP; i--)
+		printf("%d ", pas[i]);
+	printf("\n");
+}
+
+int base(int L, int BP, int *pas)
+{
+	int ctr = L;
+	int rtn = BP;
+	while (ctr > 0)
+	{
+		rtn = pas[rtn-1];
+		ctr--;
+	}
+	return rtn;
+}
+
+void execute_program(instruction *code, int printFlag)
+{
+	// variables
+	int *pas = calloc(MAX_PAS_LENGTH, sizeof(int));
+	int IC, line;
+	
+	// read in program
+	IC = 0;
+	line = 0;
+	while (code[line].opcode != -1)
+	{
+		pas[IC] = code[line].opcode;
+		IC++;
+		pas[IC] = code[line].l;
+		IC++;
+		pas[IC] = code[line].m;
+		IC++;
+		line++;
+	}
+	
+	// this will print the header in the output, make sure you put your
+	//		execution loop after it
+	if (printFlag)
+	{
+		printf("\t\t\t\tPC\tBP\tSP\tDP\tdata\n");
+		printf("Initial values:\t\t\t%d\t%d\t%d\t%d\n", PC, BP, SP, DP);
+	}
+	
+	free(pas);
+}
